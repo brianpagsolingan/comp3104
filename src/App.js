dcad13fbd90bd1baba1a16c0ff7797da86bbb185
brianpagsolingan <120866601+brianpagsolingan@users.noreply.github.com>
2025-10-14 00:11:07 -0400

    Dep inclusions (#4)
    * github action for github pages added
    
    * fixed
    
    * Name change

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Student Information System</h1>
-      <h5>Developed By: 101490258 || Brian Pagsolingan || DevOps</h5>
+      <h5>Developed By: 101490258 || Mr. Brian Pagsolingan || COMP3104 - DevOps</h5>
       <StudentForm addStudent={addStudent} />
       <StudentList students={students} />
     </div>
